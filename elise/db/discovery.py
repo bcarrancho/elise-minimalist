8b54eca778ee4fadb8aac5c7303cdd3616a696b4
Bruno Carrancho <bruno.carrancho@gmail.com>
2017-02-25 04:50:38 +0100

    Eliminated need for sql_utils

--- elise/db/discovery.py
+++ elise/db/discovery.py
@@ -2,36 +2,6 @@ import os.path
 import sqlite3
 
 
-import sqlalchemy
-import sqlalchemy_utils
-import sqlalchemy.orm
-import elise.type.common
-
-class DiscoveryDB(object):
-
-    def __init__(self, db_connector):
-        _sa_bind_typesystem()
-        self.db_connector = db_connector
-        self.engine = sqlalchemy.create_engine(self.db_connector)
-        if not sqlalchemy_utils.database_exists(self.engine.url):
-            sqlalchemy_utils.create_database(self.engine.url)
-        elise.type.common.BaseDB.metadata.create_all(self.engine)
-        self.session_factory = sqlalchemy.orm.sessionmaker(bind=self.engine)
-        self.Session = sqlalchemy.orm.scoped_session(self.session_factory)
-
-    def close(self):
-        self.Session.remove()
-        self.session_factory.close_all()
-        self.engine.dispose()
-
-
-def _sa_bind_typesystem():
-    import elise.type.matchlist
-    import elise.type.match
-    elise.type.matchlist._sa_bind_all_discovery_db()
-    elise.type.match._sa_bind_all()
-
-
 def init_discovery_db_matchlist(db_filename):
     if not os.path.isfile(db_filename):
         conn = sqlite3.connect(db_filename)
